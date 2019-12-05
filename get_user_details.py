15aa5abb7e7b5da9611368fae35e2ed4d4a227e8
Justin Sitarz <55627613+justinsitarz@users.noreply.github.com>
2019-12-04 23:33:38 -0600

    update user csv path variable

--- get_user_details.py
+++ get_user_details.py
@@ -5,7 +5,7 @@ import pandas as pd
 import sys
 
 api_key = '<your api key>'
-user_data_csv = './elsevier-user-list.csv' 
+user_data_csv = './user-list.csv' # set this to your desired path or leave the same to generate csv in same directory
 api_headers = {'Content-Type': 'application/json','Authorization':'GenieKey ' + api_key}
 get_user_url = "https://api.opsgenie.com/v2/users/" # set as https://api.eu.opsgenie.com/v2/users/ if account is in the EU region
 expand_params = {'expand': 'contact'}
